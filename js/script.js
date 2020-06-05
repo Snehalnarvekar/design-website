8516f796ee38eaa91afe42445d7b2e0f3ef81117
Snehalnarvekar <snehaln88@gmail.com>
2020-06-05 15:49:13 +0530

    first-commit

--- js/script.js
+++ js/script.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
 		$('.rules_wrapper .list_menu_wrap').slideToggle(300);
 		$(this).toggleClass('rotate');
 	});
+	$('#filter').on('click',function(){
+		$('.overlay').css('display','block');
+		$('.filter_box_menu').css('display','initial');
+	});
+	$('.overlay').on('click',function(){
+		$('.overlay').css('display','none');
+		$('.filter_box_menu').css('display','none');
+	});
 });
